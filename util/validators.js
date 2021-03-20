c42af96215a855e9b6c84889bece9b7ca0b438e8
Nick Roma <nikoandpiko@gmail.com>
2021-03-20 22:50:50 +0900

    Combines server and client for Github repository

---
+++ util/validators.js
@@ -0,0 +1,44 @@
+module.exports.validateRegisterInput = (
+  username,
+  email,
+  password,
+  confirmPassword
+) => {
+  const errors = {};
+  if (username.trim() === "") {
+    errors.username = "Username must not be left blank";
+  }
+  if (email.trim() === "") {
+    errors.email = "Email must not be left blank";
+  } else {
+    const regEx = /^([0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,9})$/;
+    if (!email.match(regEx)) {
+      errors.email = "Please enter a valid email address";
+    }
+  }
+  if (password === "") {
+    errors.password = "Password must not be left blank";
+  } else if (password !== confirmPassword) {
+    errors.confirmPassword = "Passwords must match";
+  }
+
+  return {
+    errors,
+    valid: Object.keys(errors).length < 1,
+  };
+};
+
+module.exports.validateLoginInput = (username, password) => {
+  const errors = {};
+  if (username.trim() === "") {
+    errors.username = "Username must not be left blank";
+  }
+  if (password.trim() === "") {
+    errors.password = "Password must not be left blank";
+  }
+
+  return {
+    errors,
+    valid: Object.keys(errors).length < 1,
+  };
+};
